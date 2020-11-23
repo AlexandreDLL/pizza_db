84a92012eff0f42f0d6cce33ceace454e0061a51
Alexandre Didillon <didillon.alexandre@gmail.com>
2020-11-23 10:24:42 +0100

    step-5

--- app/model/Model.js
+++ app/model/Model.js
@@ -1,16 +1,71 @@
 class Model {
 
-    assign(obj){
-        for(let v in obj){
-            if(this[v] != undefined && typeof this[v] == 'number'){
+    assign(obj) {
+        for (let v in obj) {
+            if (this[v] != undefined && typeof this[v] == 'number') {
                 this[v] = Number(obj[v]);
             }
-            else if(this[v] != undefined && typeof this[v] == 'boolean'){
-                this[v] = obj[v] == '1' ? true : false;
+            else if (this[v] != undefined && typeof this[v] == 'boolean') {
+                this[v] = obj[v] == ('1' || true) ? true : false;
             }
-            else{
+            else {
                 this[v] = obj[v];
             }
         }
+    }
+
+    insert() {
+        let table = this.constructor.name.toLowerCase();
+        let params = this;
+        let deferred = $.Deferred();
+        Rest.insert(table, params).done((resp) => {
+            let json = resp.tryJsonParse();
+            if (json) {
+                params.assign({ id: json });
+                deferred.resolve(json);
+            }
+            else {
+                deferred.reject(resp);
+            }
+        }).fail(() => {
+            deferred.reject(resp);
+        })
+        return deferred.promise();
+    }
+
+    update() {
+        let table = this.constructor.name.toLowerCase();
+        let params = this;
+        let deferred = $.Deferred();
+        Rest.update(table, params).done((resp) => {
+            let json = resp.tryJsonParse();
+            if (json) {
+                deferred.resolve(json);
+            }
+            else {
+                deferred.reject(resp);
+            }
+        }).fail(() => {
+            deferred.reject(resp);
+        })
+        return deferred.promise();
+    }
+
+    delete() {
+        let table = this.constructor.name.toLowerCase();
+        let id = this.id;
+        let deferred = $.Deferred();
+        Rest.delete(table, id).done((resp) => {
+            let json = resp.tryJsonParse();
+            if (json) {
+                deferred.resolve(json);
+            }
+            else {
+                deferred.reject(resp);
+            }
+        }).fail(() => {
+            deferred.reject(resp);
+        })
+        return deferred.promise();
     }
 }
